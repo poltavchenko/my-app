6299950349048310fb2a47e2c8499e4198039eef
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2023-07-26 15:59:36 +0300

    Responding to Events

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 27,
-      text: '+++'
+      position: ''
     }
   }
 
@@ -17,13 +17,28 @@ class WhoAmI extends Component {
     }))
   }
 
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
   render() {
     const {name, surname, link} = this.props;
+
+    const {position, years} = this.state;
+
     return (
       <div>
         <button onClick={this.nextYear} >{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
-        <a href={link}>My profile</a>
+        <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
+        <div>
+          <a href={link}>My profile</a>
+        </div>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </form>
       </div>
     )
   }
